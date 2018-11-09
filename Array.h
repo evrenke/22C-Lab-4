77dff3c4552f8216642be70eeee50e5ac016a7ce
evrenke <evrenkeskin300@gmail.com>
2018-11-09 15:03:41 -0800

    Update Array.h

--- Array.h
+++ Array.h
@@ -1,1 +1,38 @@
+#pragma once
 
+template <class ObjectType>
+class Array
+{
+private:
+	int length;
+	ObjectType *objectArray;
+public:
+
+	Array(int setLength)
+	{
+		length = setLength;
+		objectArray = new ObjectType[setLength];
+	}
+	~Array()
+	{
+
+	}
+
+	ObjectType &get(int get)
+	{
+		return *(objectArray + get);
+	}
+	void set(int set, ObjectType data)
+	{
+		*(objectArray + set) = data;
+	}
+	int getLength()
+	{
+		return length;
+	}
+
+	ObjectType &operator[] (int x)
+	{
+		return get(x);
+	}
+};
